a9812900ed6c8c3ed8b000fb891323dd8e79611a
tobe <583590744@qq.com>
2024-11-26 20:46:01 +0800

    gsap错位测试

---
+++ src/gsap/GsapHidden.tsx
@@ -0,0 +1,143 @@
+import gsap from 'gsap';
+import { Pane } from 'tweakpane';
+import { Tween, Group } from '@tweenjs/tween.js';
+
+export const GsapHidden = () => {
+  useEffect(() => {
+    const duration = 60;
+    const distanceX = 600;
+
+    const gsapHistory: number[] = [];
+    const rafHistory: number[] = [];
+    const tweenJSHistory: number[] = [];
+    const recordDur = 2 * 1000;
+    let gsapRecordTime = -1;
+    let rafRecordTime = -1;
+    let tweenJSRecordTime = -1;
+    let recordFinishCount = 0;
+
+    let hidden = document.hidden;
+    document.addEventListener('visibilitychange', () => {
+      hidden = document.hidden;
+      console.log('hidden', hidden);
+      if (hidden) {
+        if (gsapRecordTime === -1) {
+          gsapRecordTime = Date.now();
+        }
+        if (rafRecordTime === -1) {
+          rafRecordTime = Date.now();
+        }
+        if (tweenJSRecordTime === -1) {
+          tweenJSRecordTime = Date.now();
+        }
+      }
+    });
+
+    console.log('hidden', hidden);
+
+    const checkRecordFinish = () => {
+      console.log('recordFinishCount', recordFinishCount);
+      if (recordFinishCount >= 3) {
+        console.log('gsapHistory', gsapHistory);
+        console.log('rafHistory', rafHistory);
+        console.log('tweenJSHistory', tweenJSHistory);
+      }
+    };
+
+    // gsap
+    const gsapTween = gsap.to(document.getElementById('test'), {
+      duration,
+      x: distanceX, // 增加移动距离
+      onUpdate: function () {
+        const deltaSec = (Date.now() - gsapRecordTime) / 1000;
+        if (gsapRecordTime > 0) {
+          if (Date.now() - gsapRecordTime < recordDur) {
+            gsapHistory.push(deltaSec);
+          } else {
+            gsapRecordTime = -2;
+            recordFinishCount++;
+            checkRecordFinish();
+          }
+        }
+        const progress = gsapTween.progress();
+        document.getElementById('test-gsap')!.style.left =
+          `${progress * distanceX}px`;
+      },
+    });
+
+    // raf
+    const rafStartTime = Date.now();
+    const rafFunc = () => {
+      if (rafRecordTime > 0) {
+        const deltaSec = (Date.now() - rafRecordTime) / 1000;
+        if (Date.now() - rafRecordTime < recordDur) {
+          rafHistory.push(deltaSec);
+        } else {
+          rafRecordTime = -2;
+          recordFinishCount++;
+          checkRecordFinish();
+        }
+      }
+      const progress = (Date.now() - rafStartTime) / (duration * 1000);
+      document.getElementById('test-raf')!.style.left =
+        `${progress * distanceX}px`;
+      requestAnimationFrame(rafFunc);
+    };
+    rafFunc();
+
+    // tweenjs
+    const tweenGroup = new Group();
+    const tweenJSLoop = () => {
+      tweenGroup.update();
+      requestAnimationFrame(tweenJSLoop);
+    };
+    tweenJSLoop();
+
+    const tweenObj = { x: 0 };
+    const tween = new Tween(tweenObj, tweenGroup)
+      .to({ x: 600 }, duration * 1000)
+      .onUpdate(({ x }) => {
+        if (tweenJSRecordTime > 0) {
+          if (Date.now() - tweenJSRecordTime < recordDur) {
+            const deltaSec = (Date.now() - tweenJSRecordTime) / 1000;
+            tweenJSHistory.push(deltaSec);
+          } else {
+            tweenJSRecordTime = -2;
+            recordFinishCount++;
+            checkRecordFinish();
+          }
+        }
+
+        document.getElementById('test-tweenjs')!.style.left = `${x}px`;
+      });
+    win.tweenObj = tweenObj;
+    tween.start();
+  }, []);
+
+  const getContent = () => {
+    const textArr = ['gsap', 'raf', 'tweenjs'];
+    const idArr = ['test-gsap', 'test-raf', 'test-tweenjs'];
+    return idArr.map((id, idx) => {
+      const el = document.getElementById(id);
+      return (
+        <div>
+          <div
+            id={id}
+            style={{
+              width: '20px',
+              height: '20px',
+              background: 'red',
+              position: 'absolute',
+              left: 0,
+              top: idx * 100,
+            }}
+          >
+            {textArr[idx]}
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return <>{getContent()}</>;
+};
---
+++ src/gsap/GsapHiddenCanvas.tsx
@@ -0,0 +1,123 @@
+import gsap from 'gsap';
+import { Pane } from 'tweakpane';
+
+export const GsapHiddenCanvas = () => {
+  useEffect(() => {
+    // 最多记录的时间长度
+    const duration = 5 * 1000;
+    const width = 3000;
+    const height = 600;
+    const dpr = window.devicePixelRatio;
+    const cssWidth = Math.round(width / dpr);
+    const cssHeight = Math.round(height / dpr);
+    const lineHeight = 80;
+    const lineGap = 4;
+    const heightCount = 2;
+    const hGap = height / (heightCount + 1);
+
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.width = `${cssWidth}px`;
+    canvas.style.height = `${cssHeight}px`;
+
+    const gsapHistory: number[] = [];
+    const rafHistory: number[] = [];
+
+    let gsapStart = Date.now();
+    let rafStart = Date.now();
+
+    let hidden = document.hidden;
+    let preHidden = hidden;
+    document.addEventListener('visibilitychange', () => {
+      hidden = document.hidden;
+      if (hidden) {
+        preHidden = true;
+      }
+    });
+
+    const draw = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      const heightHalf = lineHeight / 2;
+      const gapHalf = lineGap / 2;
+      let startY = hGap;
+
+      for (let i = 0; i < gsapHistory.length; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'red';
+        let t = (gsapHistory[i] - gsapStart) / duration;
+        t = Math.max(0, Math.min(1, t));
+        const x = t * width;
+        ctx.moveTo(x, startY - heightHalf);
+        ctx.lineTo(x, startY + heightHalf);
+        ctx.stroke();
+      }
+
+      startY += hGap;
+
+      for (let i = 0; i < rafHistory.length; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'green';
+        let t = (rafHistory[i] - rafStart) / duration;
+        t = Math.max(0, Math.min(1, t));
+        const x = t * width;
+        ctx.moveTo(x, startY - gapHalf);
+        ctx.lineTo(x, startY + gapHalf);
+        ctx.stroke();
+      }
+    };
+
+    const tween = gsap.to(
+      {},
+      {
+        duration,
+        x: 600, // 增加移动距离
+        onUpdate: function () {
+          const now = Date.now();
+          // 超出时间，移除最早的记录
+          if (now - gsapStart > duration) {
+            if (gsapHistory.length) {
+              gsapHistory.shift();
+              if (gsapHistory.length) {
+                gsapStart = gsapHistory[0];
+              }
+            }
+          }
+          gsapHistory.push(now);
+        },
+      }
+    );
+
+    const rafFunc = () => {
+      const now = Date.now();
+      if (now - rafStart > duration) {
+        if (rafHistory.length) {
+          rafHistory.shift();
+          if (rafHistory.length) {
+            rafStart = rafHistory[0];
+          }
+        }
+      }
+      rafHistory.push(now);
+      requestAnimationFrame(rafFunc);
+    };
+    rafFunc();
+
+    let paused = false;
+    const drawLoop = () => {
+      if (paused) return;
+      draw();
+      requestAnimationFrame(drawLoop);
+    };
+    drawLoop();
+
+    const pane = new Pane();
+    pane.addButton({ title: '暂停' }).on('click', () => {
+      paused = !paused;
+      console.log('paused', paused);
+    });
+  }, []);
+
+  return <canvas style={{ border: '1px solid #ccc' }} />;
+};
