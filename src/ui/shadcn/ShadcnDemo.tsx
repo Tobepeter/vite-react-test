0ea262730116653308c273fcae39d90a92a6c63e
tobe <583590744@qq.com>
2025-04-07 12:53:06 +0800

    test shadcn

--- src/ui/shadcn/ShadcnDemo.tsx
+++ src/ui/shadcn/ShadcnDemo.tsx
@@ -1,9 +1,11 @@
 import { ShadcnButton } from './ShadcnButton'
 import { ShadcnSimple } from './ShadcnSimple'
 import { ShadcnCard } from './ShadcnCard'
+import { ShadcnCheckBox } from './ShadcnCheckBox'
 
 export const ShadcnDemo = () => {
   // return <ShadcnButton />
   // return <ShadcnSimple />
-  return <ShadcnCard />
+  // return <ShadcnCard />
+  return <ShadcnCheckBox />
 }
